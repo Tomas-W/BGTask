08badccc82e9a02d25c781eb7362dd7c2b69f62c
Tomas <tomas@git>
2025-05-15 17:42:41 +0200

    Dim service logger color

--- service/service_logger.py
+++ service/service_logger.py
@@ -1,12 +1,22 @@
+import logging
 import sys
 
 from loguru import logger as loguru_logger
 
 
 logger = loguru_logger
-custom_format = "[<green>{time:HH:mm:ss.SS}</green>] {file}:{function}:{line} - <level>{message}</level>"
+custom_format = (
+    "[<dim><green>{time:HH:mm:ss.SS}</green></dim>] "
+    "<dim><white>{file}:{function}:{line}</white></dim> - "
+    "<dim><level>{message}</level></dim>"
+)
+
 logger.remove()
 logger.add(sys.stderr,
            level="TRACE",
            format=custom_format,
            colorize=True)
+
+# Suppress
+pil_logger = logging.getLogger("PIL")
+pil_logger.setLevel(logging.WARNING)
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,7 +5,18 @@ from loguru import logger as loguru_logger
 
 
 logger = loguru_logger
-custom_format = "[<green>{time:HH:mm:ss.SS}</green>] {file}:{function}:{line} - <level>{message}</level>"
+custom_format = (
+    "[<green>{time:HH:mm:ss.SS}</green>] "
+    "<white>{file}:{function}:{line}</white> - "
+    "<level>{message}</level>"
+)
+
+logger.remove()
+logger.add(sys.stderr,
+           level="TRACE",
+           format=custom_format,
+           colorize=True)
+
 logger.remove()
 logger.add(sys.stderr,
            level="TRACE",
