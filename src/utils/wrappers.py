886e845556ff9126f89279b35757b0ba19d5ed31
Tomas <tomas@git>
2025-06-06 22:52:29 +0200

    Add @loag_time() wrapper

---
+++ src/utils/wrappers.py
@@ -0,0 +1,23 @@
+import functools
+
+from src.utils.timer import TIMER
+from src.utils.logger import logger
+
+
+def log_time(name):
+    """
+    Decorator to log duration of a method or function.
+    Usage: @log_time("name_to_time")
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            TIMER.start(name)
+            result = func(self, *args, **kwargs)
+            TIMER.stop(name)
+            
+            logger.timing(f"Loading {name} took: {TIMER.get_time(name.lower())}")
+            return result
+        return wrapper
+    return decorator
+
